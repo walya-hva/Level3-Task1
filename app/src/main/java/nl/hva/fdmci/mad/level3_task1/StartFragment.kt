3553f7136eebca5d8b72c6b41f42db0bb7760974
Achmed Waly <shyzus@Home-Workstation.home>
2020-09-13 13:37:37 +0200

    Initial commit

---
+++ app/src/main/java/nl/hva/fdmci/mad/level3_task1/StartFragment.kt
@@ -0,0 +1,30 @@
+package nl.hva.fdmci.mad.level3_task1
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_start.*
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class StartFragment : Fragment() {
+
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_start, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        btn_start_rating.setOnClickListener {
+            findNavController().navigate(R.id.action_startFragment_to_ratingFragment)
+        }
+    }
+}
---
+++ app/src/main/java/nl/hva/fdmci/mad/level3_task1/SummaryFragment.kt
@@ -0,0 +1,41 @@
+package nl.hva.fdmci.mad.level3_task1
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_summary.*
+
+const val ARG_GAME_NAME = "arg_game_name"
+const val ARG_GAME_RATING = "arg_game_rating"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [SummaryFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class SummaryFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_summary, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val gameName = arguments?.getString(ARG_GAME_NAME)
+        val gameRating = arguments?.getFloat(ARG_GAME_RATING)
+
+        txt_summary.text = String.format("You rated %s with %.1f stars! Thanks?", gameName, gameRating)
+
+        btn_start_over.setOnClickListener {
+            findNavController().navigate(R.id.action_summaryFragment_to_startFragment)
+        }
+    }
+}
